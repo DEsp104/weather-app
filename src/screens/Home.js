4afb244dc41797e58ae07f6c37e22d551d3aae9f
David Espinal <despinal0425@gmail.com>
2021-08-11 19:40:30 -0400

    added .env for api and base url as well into putting it in the gitignore

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,6 +1,15 @@
-import React from 'react'
+import React from 'react';
+import dotenv from 'dotenv';
+dotenv.config();
+
+
+
 
 function Home() {
+
+  // `${process.env.API_BASE_URL}
+  // process.env.API_KEY
+
   return (
     <div>
       
